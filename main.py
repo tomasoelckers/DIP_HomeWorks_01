c7fca40696bf1d1a3b02287f9d471dd142530e79
Tomas Oelckers <toelckers@miuande.cl>
2019-08-18 18:13:14 -0400

    Where is Waldo?

--- main.py
+++ main.py
@@ -1,12 +1,24 @@
+from scipy import signal
+import scipy as ssc
+import numpy as np
 import cv2
-
 
 DIR = '/home/tomas/Documents/Universidad/DIP/HW 1/Where_is_Waldo/vwru869.jpg'
 
-image = cv2.imread(DIR)
+image1 = cv2.imread('Images/wally2_1.jpg', 0)
+image2 = cv2.imread('Images/wally02.jpg', 0)
 
 cv2.namedWindow('Where is Waldo', cv2.WINDOW_NORMAL)
 cv2.resizeWindow('Where is Waldo', 800,600)
-cv2.imshow('Where is Waldo', image)
+cv2.imshow('Where is Waldo', image2)
+
+print('Images Imported')
+
+result = signal.correlate2d(image2, image1)
+
+print('Correlated Images ')
+
+cv2.imshow('Where is Waldo', result)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
