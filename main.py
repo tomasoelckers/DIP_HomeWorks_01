2f32ed797f9c4dee5f44af5765abd8e578d21c82
Tomas Oelckers <toelckers@miuande.cl>
2019-08-21 12:41:34 -0400

    Add more wally to a picture and finding them with a threshold equal to 0.9

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import cv2
 
 # Load the Waldo and puzzle images
 waldo = cv2.imread('Images/wally2_1.jpg')
-puzzle = cv2.imread('Images/wally2.jpg')
+puzzle = cv2.imread('Images/wally22.jpg')
 
 # Get the shape of Waldo's face image
 (waldoHeight, waldoWidth) = waldo.shape[:2]
@@ -14,11 +14,18 @@ puzzle = cv2.imread('Images/wally2.jpg')
 # cv2.TM_CCORR_NORMED is the cross-correlation normalized between the two images
 result = cv2.matchTemplate(puzzle, waldo, cv2.TM_CCORR_NORMED)
 
-# Get the Min and Max local of the cross-correlation normalized
-(_, _, minLoc, maxLoc) = cv2.minMaxLoc(result)
+# Set the threshold for find all the local max of the result
+threshold = 0.9
 
-# Draw rectangle where is waldo
-cv2.rectangle(puzzle, maxLoc, (maxLoc[0] + waldoWidth, maxLoc[1] + waldoHeight), (0,255,0))
+# Find local max on the image where the pixel value was equal or bigger than the threshold
+loc = np.where( result >= threshold)
+
+# Get the Min and Max local of the cross-correlation normalized
+(minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(result)
+
+# Draw rectangles where is waldo
+for pixel in zip(*loc[::-1]):
+    cv2.rectangle(puzzle, pixel, (pixel[0] + waldoWidth, pixel[1] + waldoHeight), (0, 0, 0), 2)
 
 # Display the result of the Where is Waldo? problem
 cv2.namedWindow('Where is Waldo', cv2.WINDOW_NORMAL)
